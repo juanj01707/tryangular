53d0ec3a071c9371368261bc5747bbeacf9530fd
MiguelRiosT <miguelriostangarife@gmail.com>
2023-10-23 21:33:43 -0500

    AvanceClase23OCT

--- src/app/register/model/register.ts
+++ src/app/register/model/register.ts
@@ -1,7 +1,12 @@
+import { Codigotipoidentificacion } from "./codigotipoidentificacion"; 
+
+
 export class Register{
 
     nombre !: string;
-    correo !: string;
-    contrasena !: string;
+    identificacion !: number;
+    codigotipoidentificacion !: Codigotipoidentificacion;  
+    
+
 
   }
