5d3126e40bde65e8c78a7258f7480d9b74693a04
juanjodiaz17 <jjde2001@gmail.com>
2023-10-26 19:45:07 -0500

    RouterLink

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
+
 import { LoginComponent } from './login.component';
 import { FormsModule } from '@angular/forms';
 
